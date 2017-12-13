75c81c0337ba1d10db9714bebfc2da0e0ebf65fb
Konstantinos Kagkelidis <kaggis@gmail.com>
2017-12-13 13:44:00 +0200

    ARGO 960 Refactor rule generator for using notifications tag

--- tests/test_argoalert.py
+++ tests/test_argoalert.py
@@ -4,37 +4,43 @@ from argoalert import argoalert
 # Test the transformation of argo endpoint group status event to alerta alert representation
 def test_endpoint_group_event():
     argo_str='{"status":"OK","endpoint_group":"SITEA","metric":"httpd.memory","service":"httpd","hostname":"webserver01","summary":"foo","type":"endpoint_group"}'
-    exp_str = '{"resource": "SITEA", "severity": "ok", "service": ["endpoint_group"], "text": "foo", "environment": "devel", "event": "status"}'
+    exp_str = '{"environment": "devel", "event": "status", "resource": "SITEA", "service": ["endpoint_group"], "severity": "ok"}'
     argo_json = json.loads(argo_str)
     alerta_json = argoalert.transform(argo_json,"devel")
-    alerta_str = json.dumps(alerta_json)
+    alerta_str = json.dumps(alerta_json, sort_keys=True)
+
     assert(alerta_str == exp_str)
 
 # Test the transformation of argo service status event to alerta alert representation
 def test_service_event():
     argo_str='{"status":"OK","endpoint_group":"SITEA","metric":"httpd.memory","service":"httpd","hostname":"webserver01","summary":"foo","type":"service"}'
-    exp_str = '{"resource": "SITEA/httpd", "severity": "ok", "service": ["service"], "text": "foo", "environment": "devel", "event": "status"}'
+    exp_str = '{"environment": "devel", "event": "status", "resource": "SITEA/httpd", "service": ["service"], "severity": "ok"}'
     argo_json = json.loads(argo_str)
-    alerta_json = argoalert.transform(argo_json,"devel")
-    alerta_str = json.dumps(alerta_json)
+    alerta_json = argoalert.transform(argo_json, "devel")
+    alerta_str = json.dumps(alerta_json, sort_keys=True)
+
     assert(alerta_str == exp_str)
 
 # Test the transformation of argo endpoint status event to alerta alert representation
 def test_endpoint_event():
     argo_str='{"status":"OK","endpoint_group":"SITEA","metric":"httpd.memory","service":"httpd","hostname":"webserver01","summary":"foo","type":"endpoint"}'
-    exp_str = '{"resource": "SITEA/httpd/webserver01", "severity": "ok", "service": ["endpoint"], "text": "foo", "environment": "devel", "event": "status"}'
+    exp_str = '{"environment": "devel", "event": "status", "resource": "SITEA/httpd/webserver01", "service": ["endpoint"], "severity": "ok"}'
     argo_json = json.loads(argo_str)
-    alerta_json = argoalert.transform(argo_json,"devel")
-    alerta_str = json.dumps(alerta_json)
+    alerta_json = argoalert.transform(argo_json, "devel")
+    alerta_str = json.dumps(alerta_json, sort_keys=True)
+
     assert(alerta_str == exp_str)
 
 # Test the transformation of argo metric status event to alerta alert representation
 def test_endpoint_metric_event():
     argo_str='{"status":"OK","endpoint_group":"SITEA","metric":"httpd.memory","service":"httpd","hostname":"webserver01","summary":"foo","type":"metric"}'
-    exp_str = '{"resource": "SITEA/httpd/webserver01/httpd.memory", "severity": "ok", "service": ["metric"], "text": "foo", "environment": "devel", "event": "status"}'
+    exp_str = '{"environment": "devel", "event": "status", "resource": "SITEA/httpd/webserver01/httpd.memory", "service": ["metric"], "severity": "ok"}'
     argo_json = json.loads(argo_str)
-    alerta_json = argoalert.transform(argo_json,"devel")
-    alerta_str = json.dumps(alerta_json)
+    alerta_json = argoalert.transform(argo_json, "devel")
+    alerta_str = json.dumps(alerta_json, sort_keys=True)
+
+    print alerta_str
+
     assert(alerta_str == exp_str)
 
 # Test gocdb xml to contacts json transformation
@@ -51,6 +57,7 @@ def test_gocdb_to_contacts():
         with open(xmlfn, 'r') as xmlfile:
             xmldata = xmlfile.read().replace('\n', '')
             contacts = argoalert.gocdb_to_contacts(xmldata)
+            print contacts
             assert (contacts == expJson)
 
 # Test contacts to alerta transformation
@@ -68,8 +75,8 @@ def test_contacts_to_alerta():
             contactData = contactJson.read().replace('\n', '')
             contacts = json.loads(contactData)
             rules = argoalert.contacts_to_alerta(contacts)
-            rulesOut = json.dumps(rules,sort_keys=True)
-            expOut = json.dumps(expJson,sort_keys=True)
+            rulesOut = json.dumps(rules, sort_keys=True)
+            expOut = json.dumps(expJson, sort_keys=True)
             assert(rulesOut == expOut)
 
 
